0785b3b41623565971ce63757e4b8869db099d7c
Ashes9997 <ayesha.sharif9997@gmail.com>
2024-05-11 13:47:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ const inches = document.querySelector(".inches");
 const result = document.querySelector(".result");
 const BMIstatus = document.querySelector(".BMIstatus");
 
+// function for validation 
+
+function validate() {
+
+    if (weight.value <=1) {
+        alert("Please enter a valid Weight");
+        weight.focus();
+    }
+    if (feet.value ==0 && inches.value == 0) {
+        alert("Please enter a valid height");
+        feet.focus();
+    }
+    else{
+        calculateBMI()
+
+    }
+  
+    
+    
+  }
+
+
 function calculateBMI() {
     let convertedWeight = weight.value;
     let status;
@@ -23,16 +45,21 @@ function calculateBMI() {
 
     // Showing the BMI status
     if (BMI <= 18.4) {
-        status = "Underweight";
+        result.textContent = `your BMI value is ${BMI.toFixed(2)} which means you are Underweight`
+
+        // status = "Underweight";
     } else if (BMI >= 18.5 && BMI <= 24.9) {
-        status = "Normal";
+        result.textContent = `your BMI value is ${BMI.toFixed(2)} which means you are Normal`
+        // status = "Normal";
     }
     else if (BMI >= 25.0 && BMI <= 39.9) {
-        status = "OverWeight";
+        result.textContent = `your BMI value is ${BMI.toFixed(2)} which means you are Overweight`
+        // status = "OverWeight";
     }
     else if (BMI >= 40.0) {
-        status = "Obese";
+        result.textContent = `your BMI value is ${BMI.toFixed(2)} which means you are Obese`
+        // status = "Obese";
     }
 
-    BMIstatus.textContent = status;
+    // BMIstatus.textContent = result;
 }
